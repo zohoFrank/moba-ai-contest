43b21878b1b74f02752a7bfd23bb8117172538a6
zohoFrank <zuoh14@mails.tsinghua.edu.cn>
2016-03-23 11:17:08 +0800

    Observer implementated

--- ai_e001.cpp
+++ ai_e001.cpp
@@ -12,28 +12,30 @@ using namespace std;
  * Const values
  ************************************************************/
 static const char* HERO_NAME[] = {"Hammerguard", "Master", "Berserker", "Scouter"};
+static const Pos KEY_POINTS[] = {Pos(75, 146), Pos(116, 114), Pos(136, 76), Pos(117, 33),
+                                 Pos(75, 23), Pos(36, 37), Pos(27, 76), Pos(35, 110)};
 
 // unchanged values (during the game)
 static int CAMP = -1;                       // which camp
-static int PLAYER = -1;                     // which player
-static int Round = 0;
-static const Pos KEY_POS[] = {};            // key positions
 
 /************************************************************
- * Policy data
+ * Sharing values
  ************************************************************/
-// 经决策器处理后得出的决策结果/处理后数据
-
-// global situation
-static int situation;                       // todo 是一个比值, 反映战况
 static Console* console = nullptr;
-// policy data
-static int global_state = -1;
-static vector<int> bf_fight_state;
-static vector<int> bf_tactic_state;
+
+// observer info
+static int Round = 0;
+static int my_money = 0;
+static int enemy_money = 0;
+static vector<PUnit*> current_friends;
+static vector<PUnit*> vi_enemies;
+// commander order
+static vector<Pos> needBackup;              // 某些位置请求支援
+static int baser[2] = {};                   // 这3个数组为每个点分配的战斗人员数量,
+static int miner[7] = {};                   // 机制:需要[再]增加一个单位时+1,
+static int point_taker[8] = {};             // 新加入一个支援单位-1,最小可以为负,表示不需要如此多战斗人员
 
 /*################# Assistant functions ####################*/
-vector<PUnit*> sumEnemies(vector<PUnit*> friends);
 void getWeakestUnit(vector<PUnit*> units, PUnit* weakest);
 void getStrongestUnit(vector<PUnit*> units, PUnit* strongest);
 
@@ -45,14 +47,14 @@ void getStrongestUnit(vector<PUnit*> units, PUnit* strongest);
  ************************************************************/
 // 汇总所有发现的信息,便于各级指挥官决策
 // 必须每次运行均首先调用
-struct Observer {
-    int round;
-    int money;
-    // units info
-    vector<PUnit*> my_units;
-    vector<PUnit*> vi_enemies;
-    vector<PUnit*> vi_monsters;
+class Observer {
+protected:
+    // helpers
+    void getBasicInfo();
+    void getEconomyInfo();
+    void getUnits();
 
+public:
     // run observer
     void observeGame();
 };
@@ -77,54 +79,6 @@ struct Commander {
 
 
 };
-
-
-/************************************************************
- * Battle Field
- ************************************************************/
-// 1.分析战区战况.
-// 2.领任务的UnitFilter.
-// fight_state: inferior, equal, superior
-// tactic_state: pushing, hunting, mining, tempting, | fallback, stealing mines, | inspecting
-
-struct BattleField {
-    bool active;
-    int id;                             // 战区id
-    Pos* position;
-    UnitFilter* callFilter;
-    // todo 分析战区局势参数
-
-    // constructor
-    BattleField(
-            int _id,
-            bool _act = false,
-            Pos* _pos
-    ) :
-            id(_id),
-            active(_act),
-            position(_pos)
-    {}
-    ~BattleField() {
-        if (!position) {
-            delete position;
-            position = nullptr;
-        }
-        if (!callFilter) {
-            delete callFilter;
-            callFilter = nullptr;
-        }
-    }
-
-    // methods
-    void setActive(bool isActive) {
-        active = isActive;
-    }
-    void analyzeSituation();
-    void analyzeBattleFields();
-    void nearestBF(Pos* position, BattleField* nearest);      // 某位置的最近战区
-
-};
-
 
 /************************************************************
  * Heroes
@@ -166,7 +120,6 @@ public:
     void selectAction();                        // 考虑所有state的组合
 
 };
-// todo 注意删除
 
 /***************** Derived classes **************************/
 class Hammerguard : public Hero {
@@ -199,12 +152,33 @@ void player_ai(const PMap &map, const PPlayerInfo &info, PCommand &cmd) {
 
 /*################### IMPLEMENTATION #######################*/
 /************************************************************
- * Implementation: struct Observer
+ * Implementation: class Observer
  ************************************************************/
+void Observer::getBasicInfo() {
+    Round = console->round();
+    CAMP = console->camp();
+}
+
+void Observer::getEconomyInfo() {
+    my_money = console->gold();
+}
+
+void Observer::getUnits() {
+    current_friends = console->friendlyUnits();
+    vi_enemies = console->enemyUnits();
+}
+
+void Observer::observeGame() {
+    if (Round == 0)
+        getBasicInfo();
+    getEconomyInfo();
+    getUnits();
+}
+
 
 
 /************************************************************
- * Implementation: struct Commander
+ * Implementation:
  ************************************************************/
 
 /************************************************************
